5bde2779e700900e52b4c66f59ca9178dd0e1140
Christoph Jean-Luc Schneider <christoph.schneider@abineo.com>
2023-08-15 18:50:50 +0200

    rm unused binary

--- src/bin/display.rs
+++
@@ -1,8 +0,0 @@
-use auid::Uid;
-
-fn main() {
-    let int = Uid::new();
-    println!("{}", *int);
-    println!("{}", int.to_hex());
-    println!("{}", int);
-}
--- src/bin/encoding.rs
+++ src/bin/encoding.rs
@@ -1,12 +1,8 @@
-use base64::prelude::BASE64_STANDARD;
-use base64::Engine;
-
 use auid::Uid;
 
 fn main() {
-    let i = Uid::new();
-    println!("10: {}", *i);
-    println!("16: {:X}", *i);
-    println!("58: {}", i);
-    println!("64: {}", BASE64_STANDARD.encode(i.to_be_bytes()));
+    let int = Uid::new();
+    println!("{}", *int);
+    println!("{}", int.to_hex());
+    println!("{}", int);
 }
